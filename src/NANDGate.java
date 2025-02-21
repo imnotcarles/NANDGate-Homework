65681d4b939b22d228e75654b7c278f9a49d3c63
unknown <342859170@TDSB.ON.CA>
2025-02-21 11:38:12 -0500

    Final code for NANDGate Homework

--- src/NANDGate.java
+++ src/NANDGate.java
@@ -4,12 +4,12 @@ public class NANDGate {
     public static void main(String[] args) throws Exception {
         Scanner keyboard = new Scanner(System.in);
     
-        System.out.println("Please enter an a value (y/n):");
-        char a = keyboard.next().charAt(0);
-        System.out.println("Please enter a b value (y/n):");
-        char b = keyboard.next().charAt(0);
+        System.out.println("Please enter input a:");
+        boolean a = keyboard.nextBoolean();
+        System.out.println("Please enter input b:");
+        boolean b = keyboard.nextBoolean();
 
-        boolean c = !((a == 'y' || a == 'Y') && (b == 'y' || b == 'Y'));
+        boolean c = !(a && b);
 
         System.out.println("The value of c is: " + c);
     }
